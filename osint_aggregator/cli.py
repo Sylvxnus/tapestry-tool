83b9ddab7024c969dacbb4b4249d7e7d3a19aa68
aidan <aidan.h.wood@gmail.com>
2026-09-01 21:24:58 +0100

    Updated cli.py logic to implement username recon module

--- osint_aggregator/cli.py
+++ osint_aggregator/cli.py
@@ -1,5 +1,8 @@
+import json
+from dataclasses import asdict
 import click
 from .schema import Report
+from .modules import username as username_module
 
 @click.command()
 @click.option("--username", default=None)
@@ -8,8 +11,17 @@ from .schema import Report
 @click.option("--out", default="report.json")
 def recon(username, domain, email, out):
     report = Report()
-    # Just a really basic structure atm, I shall be addimg stuff later on this week :)
-    click.echo(f"Collected {len(report.findings)} findings so far.")
+
+    if username:
+        username_module.run(username, report)
+    # The next lines are for later when i implement the domain and email chaining logic
+    # if domain -> modules.domain.run(domain, report)
+    # if email -> modules.breach.run(email, report)
+
+    with open(out, "w") as f:
+        json.dump([asdict(finding) for finding in report.findings], f, indent=2)
+
+    click.echo(f"Collected {len(report.findings)} findings. Written to {out}")
 
 if __name__ == "__main__":
     recon()
